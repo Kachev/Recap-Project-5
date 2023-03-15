1512cd2f96aeee5696db55f7753794ba17077e06
YujinLu9 <wuwujane@hotmail.com>
2023-03-15 15:18:36 +0100

    button

--- components/ArtPieces/ArtPieces.js
+++ components/ArtPieces/ArtPieces.js
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import styled from "styled-components";
 import Link from "next/link";
+import Heart from "../../resources/assets/heart.svg";
+import { useState } from "react";
 
 const StyleList = styled.li`
   list-style-type: none;
 `;
 
 export default function ArtPieces({ pieces }) {
+const[favorite, setFavorite] = useState([]);
+
+const info = favorite.find((info)=> info.slug===slug)? ;
+
+function handleFavorite(){};
+
   return (
     <>
       <ul>
@@ -14,15 +22,20 @@ export default function ArtPieces({ pieces }) {
           <StyleList key={piece.slug}>
             <div>
               <Link href={`/pieces/${piece.slug}`}>
-                "{piece.name}" by {piece.artist}
+                <Image
+                  src={piece.imageSource}
+                  alt={piece.slug}
+                  width={200}
+                  height={200}
+                />
               </Link>
+              <button type="button" key={piece.slug} onClick={handleFavorite}>
+                ❤️
+              </button>
+              <p>
+                "{piece.name}" by {piece.artist}
+              </p>
             </div>
-            <Image
-              src={piece.imageSource}
-              alt={piece.slug}
-              width={200}
-              height={200}
-            />
           </StyleList>
         ))}
       </ul>
