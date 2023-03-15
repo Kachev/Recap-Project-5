4261672c4e0a30e67ba52e3bf09a0f4b525f1bf1
HristoKachev <93873677+Kachev@users.noreply.github.com>
2023-03-15 16:29:55 +0100

    Favorite Button

--- pages/pieces/[slug].js
+++ pages/pieces/[slug].js
@@ -1,11 +1,32 @@
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import ArtPieceDetails from "../../components/ArtPieceDetails/ArtPieceDetails";
-
+import { useState } from "react";
+import FavoriteButton from "../../components/Button/Favorite-button";
 export default function DetailsPage() {
   const router = useRouter();
   const { slug } = router.query;
+  const [favorites, setFavorites] = useState([]);
+  
 
+  const info = favorites.find((favorites) => favorites.slug === slug) ?? {
+    isLiked: false,
+  };
+  const { isLiked } = info;
+  function handleFavorite(slug) {
+    setFavorites((favorites) => {
+      const info = favorites.find((info) => info.slug === slug);
+      
+      if (info) {
+        return favorites.map((info) =>
+          info.slug === slug ? { ...info, isLiked: !info.isLiked } : info
+        );
+      }
+      return [...favorites, { slug, isLiked: true }];
+    });
+  }
+
+  
   const { data, error, isLoading } = useSWR(
     "https://example-apis.vercel.app/api/art"
   );
@@ -18,10 +39,12 @@ export default function DetailsPage() {
 
   const index = data.findIndex((piece) => piece.slug === slug);
   const piece = data[index];
+
   //console.log(piece);
   return (
     <main>
       <ArtPieceDetails {...piece} />
+      <FavoriteButton slug={slug} handleFavorite={handleFavorite} isLiked={isLiked}/>
     </main>
   );
 }
