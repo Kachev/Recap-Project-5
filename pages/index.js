36c072db6c803b8e26851e2490854e9be7c39e64
Kevin Koriath <kevin.koriath@gmx.de>
2023-03-15 14:07:18 +0100

    Add a Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,16 @@
 import useSWR from "swr";
+import Navbar from "../components/Nav-bar/nav-bar";
 import ListOfAll from "./pieces";
 
-
 export default function HomePage() {
-  const { data, error, isLoading, mutate } = useSWR("https://example-apis.vercel.app/api/art");
+  const { data, error, isLoading, mutate } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
   if (error) {
     console.log(error);
     return <div>Something is wrong!</div>;
   }
   if (isLoading) return <h1>Loading...</h1>;
   console.log(data);
-  return (
-    <div>
-
-      <h1>{}</h1>
-    </div>
-  );
+  return <></>;
 }
