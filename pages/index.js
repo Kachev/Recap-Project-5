42c1f2e5b067de550f824a6f83dc35006f46e755
HristoKachev <93873677+Kachev@users.noreply.github.com>
2023-03-15 14:21:13 +0100

    commit before pull

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
 import useSWR from "swr";
 import ListOfAll from "./pieces";
+import Image from "next/image";
 
-
+const URL = "https://example-apis.vercel.app/api/art";
 export default function HomePage() {
-  const { data, error, isLoading, mutate } = useSWR("https://example-apis.vercel.app/api/art");
+  const { data, error, isLoading, mutate } = useSWR(URL);
   if (error) {
     console.log(error);
     return <div>Something is wrong!</div>;
   }
   if (isLoading) return <h1>Loading...</h1>;
   console.log(data);
+
+  function RandomImage() {}
+
   return (
     <div>
       <header>
         <title>Home Page</title>
+        <h1>hshd</h1>
       </header>
-
       
     </div>
-   
   );
 }
